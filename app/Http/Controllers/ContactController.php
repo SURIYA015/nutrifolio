c54782dc5fbb80cf626338810e3a24aafda2631f
SURIYA015 <suriyaswamy18@gmail.com>
2022-01-21 14:56:03 +0530

    mail test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -40,7 +40,7 @@ class ContactController extends Controller
         //     ]);
         // }
 
-        return redirect()->route('frontend.welcome');
+        return view('frontend.welcome');
 
 
     }
